a87fd31c6e06ed7cc5cf0c3ca9352473b0204f3d
Seba Ink <sebastian@gestioncervecera.com>
2020-09-03 08:26:18 -0300

    fix guion bajo

--- src/store.js
+++ src/store.js
@@ -37,7 +37,7 @@ const makeWhatsappLink = (listaDeProductos) => {
     let link = `https://wa.me/5491167060100/?text=${encodeURI('*Pedido*\n')}`;
   
     listaDeProductos.forEach(p=> 
-        link += encodeURI(` *- ${p.cantidadAComprar} x* _${p.nombre}_ (_$ ${p.obtenerPrecioParaLaCantidadAComprar()}_) *= $ ${p.getSubTotal()}* \n`)
+        link += encodeURI(` *- ${p.cantidadAComprar} x* _${p.nombre}_ ($ ${p.obtenerPrecioParaLaCantidadAComprar()}) *= $ ${p.getSubTotal()}* \n`)
     );
     
     link += `\n . \n . \n *TOTAL* => *$ ${totalDelPedido(listaDeProductos)}*`
